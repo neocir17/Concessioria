d593414fe1c316c02bb9d5ec1e467aa7271fb6dc
neocir17 <71474496+neocir17@users.noreply.github.com>
2020-11-02 00:09:57 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+    Concessionaria meuCarro = new Concessionaria ();
+
+    nomeCarro.numvendas = 10;
+    nomeCarro.vendafinalizada =  4 ;
+    
+
+    System.out.println("Qual é sua marca preferido ? ");
+    marca = teclado.nextLine();
+          
+    meuMarca.setCarro(marca);
+    meuMarca.mostrarmarca();
+
+    nomeCarro.setnomeCarro( " volvo " );
+    system.out.println(nomeCarro.getnomeCarro());
+
+    nomeCarro.marcaCarro("caminhao volvo");
+    System.out.println(nomeCarro.getnumvendas());
+
+    System.out.println("numero de vendas: " + nomeCarro.getnumvendas );
+    System.out.println("vendas: " + nomeCarro.vendas );
+    nomeCarro.vendas();
+
+    }
+
+}
+
+    
